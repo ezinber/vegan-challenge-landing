fad1b66b8cc4886ac66f20fd1e730f1874366e86
ezinber <ezinber@duck.com>
2025-08-11 21:10:05 +0400

    feat: add animations on hover for cards and shadows for images

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
 import { registerEzHeader } from "../components/ez-header/ez-header.js";
+import { registerEzDialog } from "../components/ez-dialog/ez-dialog.js";
 
 registerEzHeader();
+registerEzDialog();
